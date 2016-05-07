3011774a65311586f5094a62dd82c6bd2e9ed7f7
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-05-07 17:41:21 -0400

    add event handler overriter

--- src/index.js
+++ src/index.js
@@ -10,3 +10,24 @@ var Consuela = function() {
 // Extending AlexaSkill
 Consuela.prototype = Object.create(AlexaSkill.prototype);
 Consuela.prototype.constructor = Consuela;
+
+Consuela.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+};
+
+Consuela.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleWelcomeRequest(response);
+};
+
+Consuela.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+};
+
+Consuela.prototype.intentHandlers = {
+  "trainStatus": function(intent, session, response) {
+    handletrainsStatusRequest(intent, session, response);
+  };
+};
