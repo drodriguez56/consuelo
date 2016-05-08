1a853047be8c3c84705bb6aafa4f79aae6730846
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-05-08 08:36:49 -0400

    added npm install modules andi setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-var APP_ID = undefined
+var APP_ID = "amzn1.echo-sdk-ams.app.2c0a5ffa-2eb3-423d-8a17-2cb38c7c3428"; 
 
-var http = require('http');
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+var data = {};
+var http = require("http");
 
-var AlexaSkill = require('./AlexaSkill');
+var AlexaSkill = require("./AlexaSkill");
+
+var STATIONS={
+  'Whitlock': '612N' ,
+  'ninetysixth': '625S'
+};
 
 var Consuela = function() {
   AlexaSkill.call(this, APP_ID);
@@ -12,8 +20,7 @@ Consuela.prototype = Object.create(AlexaSkill.prototype);
 Consuela.prototype.constructor = Consuela;
 
 Consuela.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+    console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
 };
 
 Consuela.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
@@ -22,25 +29,77 @@ Consuela.prototype.eventHandlers.onLaunch = function (launchRequest, session, re
 };
 
 Consuela.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
-        + ", sessionId: " + session.sessionId);
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
 };
 
 Consuela.prototype.intentHandlers = {
   "TrainStatus": function(intent, session, response) {
     handleTrainStatusRequest(intent, session, response);
-  };
+  },
+  "TrainTimes": function(intent, session, response) {
+    handleTrainTimeRequest(intent, session, response);
+  },
+  "AMAZON.HelpIntent": function(intent, session, response) {
+    handleHelpRequest(response);
+  }
 };
 
+function handleHelpRequest(response) {
+  var repromptText = "what train do you whant me to check?";
+  var speechOutput = "I can Help you looking for train times, "+ "or train status. " + repromptText;
+  response.ask(speechOutput, repromptText);
+};
 function handleWelcomeRequest(response) {
-  // hi, whant to check your subway status today?  
+  var speechOutput = "test";
+  var repromptText = "moretest";
+  
+  response.ask(speechOutput, repromptText);
 };
 
-function handleTrainStatusRequest (intent, session, response) {
-  var TrainStatus = 'delay';
+function handleTrainTimeRequest(intent, session, response) {
+  var speechOutput;
+ var innerResponse = response; 
+   var stationSlot = intent.slots.Station;
+   var endpoint = 'http://datamine.mta.info/mta_esi.php?key=f4958b338ff06e6f90047282ac09b812&feed_id=1';
+  var requestSettings = {
+    method: 'GET',
+    url: endpoint, 
+    encoding: null
+  };
+  request(requestSettings, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+      data = feed
+      console.log(STATIONS[stationSlot.value]);
+      var station = STATIONS[stationSlot.value];
+      console.log(station)
+      var times = parseData(station);
+      var lastTime = times.join(", ");
+      speechOutput = "train arrives in, " + lastTime + " minutes."
+      innerResponse.tellWithCard(speechOutput, "Consuela", speechOutput)
+    }
+  });
+         
+};
+
+function parseData (station) {
+ var updates = data.entity.map(function(ent) { if (ent.trip_update) { return ent.trip_update.stop_time_update } });
+	var merged = [].concat.apply([], updates);
+	var trains =  merged.filter(function(ele) { if (ele) return ele.stop_id === station } )
+	return trains.map(function(car) { return Math.round((new Date(1000 * car.arrival.time.low) - Date.now())/1000/60) } )
+}
+
+function handleTrainStatusRequest(intent, session, response) {
   var speechOutput;
 
-  speechOutput = 'hey your train is dalay. sorry.. fuck you'
+  speechOutput = "your train is dalay. sorry.. fuck you";
 
-  response.tellWithCard(speechOutput, "Consuela", speechOutput)
+  response.tellWithCard(speechOutput, "Consuela", speechOutput);
+};
+
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    var consuela = new Consuela();
+    consuela.execute(event, context);
 };
